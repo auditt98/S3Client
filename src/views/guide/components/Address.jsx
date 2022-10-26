c7300874090739e0851fd991b7abdb4b612dc34d
datpt6 <datpt6@rikkeisoft.com>
2022-10-26 18:00:17 +0700

    feat: address screeen

--- src/views/guide/components/Address.jsx
+++ src/views/guide/components/Address.jsx
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Address = () => {
-	return <div>Address</div>;
+	const [dataTable, setDataTable] = useState([
+		{ name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+	]);
+
+	return (
+		<div>
+			<div className='flex justify-between pb-3 border-b-2'>
+				<p className='text-base'>Danh sách địa chỉ nhận hàng ({'1'})</p>
+				<button className='mr-2 px-7 py-2  border border-blue-500 bg-blue-500 text-white rounded hover:bg-blue-400'>
+					Thêm địa chỉ mới
+				</button>{' '}
+			</div>
+			<div>
+				<div className='mt-5 flex flex-col'>
+					<div className='-my-2 -mx-4  sm:-mx-6 lg:-mx-8'>
+						<div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
+							<div className='overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg'>
+								<table className='min-w-full divide-y divide-gray-300'>
+									<thead className='bg-gray-50'>
+										<tr>
+											<th
+												scope='col'
+												className='py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6'
+											>
+												Họ tên
+											</th>
+											<th
+												scope='col'
+												className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
+											>
+												Điện thoại
+											</th>
+											<th
+												scope='col'
+												className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
+											>
+												Tên địa chỉ
+											</th>
+											<th
+												scope='col'
+												className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
+											>
+												Địa chỉ
+											</th>
+											<th scope='col' className='relative py-3.5 pl-3 pr-4 sm:pr-6'>
+												<span className='sr-only'></span>
+											</th>
+											<th scope='col' className='relative py-3.5 pl-3 pr-4 sm:pr-6'>
+												<span className='sr-only'></span>
+											</th>
+										</tr>
+									</thead>
+									<tbody className='divide-y divide-gray-200 bg-white'>
+										{dataTable.map((dataRow) => (
+											<tr key={dataRow.email}>
+												<td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+													{dataRow.name}
+												</td>
+												<td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
+													{dataRow.title}
+												</td>
+												<td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
+													{dataRow.email}
+												</td>
+												<td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
+													{dataRow.role}
+												</td>
+												<td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
+													<span className='bg-lime-500 rounded-full text-white p-1'>
+														Mặc định
+													</span>
+												</td>
+												<td className='relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm  sm:pr-6'>
+													<a href='#' className='text-indigo-600 hover:text-indigo-900'>
+														Edit<span className='sr-only'>, {dataRow.name}</span>
+													</a>
+												</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Address;
