e551f90cb4266610979b05e62272b6f86523213a
datpt6 <datpt6@rikkeisoft.com>
2022-10-26 18:04:06 +0700

    feat: add i18n

--- src/views/guide/Guide.jsx
+++ src/views/guide/Guide.jsx
@@ -3,48 +3,45 @@ import { Lucide, Dropdown, DropdownToggle, DropdownMenu, DropdownContent, Dropdo
 import { faker as $f } from '@/utils';
 import classnames from 'classnames';
 import { Outlet, useNavigate } from 'react-router-dom';
-// import { useTranslation } from 'react-i18next';
-
-const LIST_MENU = [
-	{
-		name: 'Thông tin tài khoản',
-		icon: '',
-		selected: true,
-		href: '/guide',
-	},
-	{
-		name: 'Địa chỉ của bạn',
-		icon: '',
-		selected: false,
-		href: '/guide/address',
-	},
-	{
-		name: 'Lịch sử giao dịch',
-		icon: '',
-		selected: false,
-		href: '/guide/transactions',
-	},
-	{
-		name: 'Hướng dẫn',
-		icon: '',
-		selected: false,
-		href: '/guide/faqs',
-	},
-];
-
-function classNames(...classes) {
-	return classes.filter(Boolean).join(' ');
-}
+import { useTranslation } from 'react-i18next';
+import { helper } from '@/utils/helper';
 
 export default function Guide() {
 	const navigate = useNavigate();
-	// const { t } = useTranslation();
+	const { t } = useTranslation();
+
+	const LIST_MENU = [
+		{
+			name: t('Thông tin tài khoản'),
+			icon: '',
+			selected: true,
+			href: '/guide',
+		},
+		{
+			name: t('Địa chỉ của bạn'),
+			icon: '',
+			selected: false,
+			href: '/guide/address',
+		},
+		{
+			name: t('Lịch sử giao dịch'),
+			icon: '',
+			selected: false,
+			href: '/guide/transactions',
+		},
+		{
+			name: t('Hướng dẫn'),
+			icon: '',
+			selected: false,
+			href: '/guide/faqs',
+		},
+	];
 
 	return (
 		<>
 			<div className='grid grid-cols-12 gap-6 mt-8'>
 				<div className='col-span-12 lg:col-span-4 2xl:col-span-4'>
-					<h2 className='intro-y text-lg font-medium mr-auto mt-2'>Thông tin tài khoản</h2>
+					<h2 className='intro-y text-lg font-medium mr-auto mt-2'>{t('Thông tin tài khoản')}</h2>
 					<div className='box p-2 mt-14 pt-14'>
 						<span className='absolute -top-10 left-[calc(50%-2.5rem)] inline-flex h-20 w-20 items-center justify-center rounded-full bg-gray-500'>
 							<span className='text-xs font-medium leading-none text-white'>TW</span>
@@ -55,7 +52,7 @@ export default function Guide() {
 							{LIST_MENU.map((menuItem, index) => (
 								<li
 									key={menuItem.name}
-									className={classNames(
+									className={helper.classNames(
 										menuItem.selected
 											? 'bg-primary-50 border-indigo-700 text-indigo-600'
 											: 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
